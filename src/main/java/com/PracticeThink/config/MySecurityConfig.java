89c130d3925c56c688286d48156fc9937185cd29
om Dubbe <dubbeom@gmail.com>
2025-07-11 14:50:17 +0530

    add new file and new configure

--- src/main/java/com/PracticeThink/config/MySecurityConfig.java
+++ src/main/java/com/PracticeThink/config/MySecurityConfig.java
@@ -2,30 +2,16 @@ package com.PracticeThink.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 	public class MySecurityConfig {
-
-		@Bean
-		public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-			http
-					.authorizeHttpRequests(auth -> auth // simple use the lambda syntax to provide rule configuration
-							.anyRequest().authenticated() //this means http request must be authenticated
-					)
-					.httpBasic(); // popup show the browser to enter username and password
-
-			return http.build(); // returns it as a spring bean
-		}
-
 		  @Bean
            public UserDetailsService userDetailsService(){
 			   UserDetails userDetails = User.builder()
@@ -40,10 +26,11 @@ import org.springframework.security.web.SecurityFilterChain;
 					  .roles("USER")
 					  .build();
 
-			   return new InMemoryUserDetailsManager(userDetails,userDetail); // it will use bean to authenticate incoming request
+			   return new InMemoryUserDetailsManager(userDetails,userDetail);
 		   }
 		   @Bean
-           public PasswordEncoder passwordEncoder(){ // simple find the ecoder
+           public PasswordEncoder passwordEncoder(){
 			return new BCryptPasswordEncoder();
+			// somthing
 	  }
 	}
---
+++ src/main/java/com/PracticeThink/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.PracticeThink.controller;
+
+import com.PracticeThink.util.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+
+	   
+}
